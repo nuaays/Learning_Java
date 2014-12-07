c066b827478319dedce6cab08c926d3bcf25b477
senya <nuaays@gmail.com>
2014-12-07 23:12:35 +0800

    add Math.random()

--- src/src/LotteryOdds.java
+++ src/src/LotteryOdds.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class LotteryOdds
 {
-    public static void main(String[] args)
+    public static void testOdds()
     {
         Scanner in = new Scanner(System.in);
         System.out.println("How Many numbers do you need to draw?");
@@ -17,4 +17,39 @@ public class LotteryOdds
 
         System.out.println("Your odds are 1 in " + lotteryOdds + ". Good Luck!");
     }
+
+    public static void testArrays()
+    {
+        int[] a = new int[100];
+        for(int i = 0; i < a.length; i++) {
+            a[i] = i;
+        }
+        for(int x : a)
+            System.out.println(x);
+
+        System.out.println(Arrays.toString(a));
+
+        int[] result = new int[6];
+        for(int j=0; j<result.length; j++)
+        {
+            int r = (int) (Math.random() * a.length);
+            result[j] = a[r];
+        }
+
+        Arrays.sort(result);
+        for(int x : result)
+            System.out.println(x);
+    }
+
+    public static void main(String[] args)
+    {
+        testArrays();
+
+        //testOdds();
+
+    }
+
+
+
+
 }
